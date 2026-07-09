59beb36c49fb9e2dd5c03587d8412dea3bd507e3
Trupti Patil <patiltrupti142006@gmail.com>
2026-07-09 13:21:45 +0530

    Fix product service

--- src/services/productService.js
+++ src/services/productService.js
@@ -1,7 +1,28 @@
 import axios from "axios";
 
-const API_URL = const API_URL = import.meta.env.VITE_API_URL + "/api/products";
+const API_URL = import.meta.env.VITE_API_URL + "/api/products";
 
+// Get all products
 export const getProducts = async () => {
   return await axios.get(API_URL);
+};
+
+// Get single product
+export const getProductById = async (id) => {
+  return await axios.get(`${API_URL}/${id}`);
+};
+
+// Add product
+export const addProduct = async (productData) => {
+  return await axios.post(API_URL, productData);
+};
+
+// Update product
+export const updateProduct = async (id, productData) => {
+  return await axios.put(`${API_URL}/${id}`, productData);
+};
+
+// Delete product
+export const deleteProduct = async (id) => {
+  return await axios.delete(`${API_URL}/${id}`);
 };
